ec704b5a05ee468789712753aae0c0821631743e
Maks11455 <maks101330@mail.ru>
2023-11-27 20:16:51 +0000

    last commit

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 string[] array1 = new string[5] {"123", "23", "hello", "world", "res"};
 string[] array2 = new string[array1.Length];
-
+{
     int count = 0;
     for (int i = 0; i < array1.Length; i++)
     {
@@ -10,3 +10,14 @@ string[] array2 = new string[array1.Length];
         count++;
         }
     }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SecondArrayWithIF(array1, array2);
+PrintArray(array2);
